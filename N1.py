26b7ddac5d9ea8d8a3bed12458a31296fa154e1c
boriama <boriama@yandex.ru>
2021-03-04 15:53:38 +0400

    first homework

---
+++ N1.py
@@ -0,0 +1,7 @@
+name = input("Введите ваше имя: ")
+age = input("Введите ваш возраст: ")
+print(f"Привет {name}, мне тоже скоро {age}!")
+number_1 = int(input("Введите число от 0-9: "))
+number_2 = int(input("Введите второе число: "))
+number_3 = number_1 + number_2
+print(f"Их сумма равна: {number_1} + {number_2} = {number_3}")
